3870ccb09e9aa9ac6a2398a5cdb8282f0d4b90c6
cosmin-pascariu <cosmin.pascariu2@student.usv.ro>
2022-07-12 16:46:37 +0300

    Add login page layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { createGlobalStyle } from "styled-components";
 
 import HomePage from "./pages/homepage/HomePage";
+import LoginPage from './pages/LoginPage/LoginPage';
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    box-sizing: border-box;
+  }
+`
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <GlobalStyle />
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path="/*" element={<Navigate replace to='/' />} />
-
+          <Route path='/login' element={<LoginPage/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/LoginPage/LoginPageElements.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+import LogoAssist from '../../assets/images/assistLogo.png';
+
+export const MainContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+`
+export const LeftSide = styled.div`
+    height: 100%;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const LoginImage = styled.img`
+    heigth: auto;
+    width: 50vw;
+`
+export const ContentContainer = styled.div`
+    width: 50%;
+    height: 75%;
+    display: flex;
+    flex-direction: column;
+`
+
+export const LoginForm = styled.form`
+`
+
+export const FormTitle = styled.h1`
+    font-size: 2rem;
+    color: #fff;
+`
