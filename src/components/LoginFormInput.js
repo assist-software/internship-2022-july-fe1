48569cf59f94607c0ba4c1374aeb7bfd37e2aa4e
cosmin-pascariu <cosmin.pascariu2@student.usv.ro>
2022-07-13 12:34:30 +0300

    [Task-Login] - create account

---
+++ src/components/LoginFormInput.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+const LoginFormInput = () => {
+  return (
+    <Input/>
+  )
+}
+
+export default LoginFormInput
+
+const Input = styled.input`
+    width: 100%;
+    height: 44px;
+    border-radius: 8px;
+    border: 1px solid #D0D5DD;
+    font-size: 16px;
+    padding: 0 20px;
+    margin: 5px 0 20px 0;
+    font-weight: 500;
+`
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { LeftSide, MainContainer, LoginImage, ContentContainer, LoginForm, BreakLine } from './LoginPageElements';
 import ImageLogin from "../../assets/images/furniture.png";
 import AssistLogo from '../../components/AssistLogo';
 import FormTitle from '../../components/FormTitle';
 import FormLabel from '../../components/FormLabel';
 import {GoogleLogin} from 'react-google-login'
+import InputLabel from '../../components/InputLabel';
+import LoginFormInput from '../../components/LoginFormInput';
+import PasswordInput from '../../components/PasswordInput';
+import FormButton from '../../components/FormButton';
+
 
 const clientId = 'YOUR_CLIENT_ID.apps.googleeusearchcontent.com';
 
 const LoginPage = () => {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [passwordVisibility, setPasswordVisibility] = useState(true);
+
+  const toogleVisibility = () => {
+    setPasswordVisibility(!passwordVisibility);
+  };
+
   const onSuccess = (res) => {
-    alert('[Login Succes] currentUser:', res.profileObj);
+    console.log('[Login Succes] currentUser:', res.profileObj);
   }
   const onFailure = (res) => {
-    alert('[Login Failed] res',res);
+    console.log('[Login Failed] res',res);
   }
 
   return (
@@ -30,7 +43,6 @@ const LoginPage = () => {
                 onSuccess={onSuccess}
                 onFailure={onFailure}
                 className={"Google"}
-                
                 cookiePolicy={'single_host_origin'}
                 style={{textAlign: 'center'}}
               />
@@ -38,6 +50,22 @@ const LoginPage = () => {
                 <hr/>
                 <p>OR</p>
               </BreakLine>
+              <InputLabel text="Email"/ >
+              <LoginFormInput
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type={"text"}
+              id="email"
+              placeholder="Email"/>
+              
+              <InputLabel text="Password"/ >
+              <PasswordInput passwordVisibility={passwordVisibility} toogleVisibility={toogleVisibility} password={password} setPassword={setPassword}/>
+              <h2>At least 8 characters and one number.</h2>
+            <FormButton text="Sign up"/>
+
+            <div className='inline'>
+            <h4>Already have an account?</h4><a href=''>Log In</a>
+            </div>
             </LoginForm>
           </ContentContainer>
         </LeftSide>
