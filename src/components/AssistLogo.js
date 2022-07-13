233ecd5f8ae378809c88c1e6c5e09e2738f84466
EmiUngurean <emiungurean1@gmail.com>
2022-07-13 10:45:48 +0300

    header first

--- src/components/AssistLogo.js
+++ src/components/AssistLogo.js
@@ -2,16 +2,21 @@ import styled from 'styled-components'
 import LogoAssist from '../assets/images/assistLogo.png';
 
 const AssistLogo = () => {
-  return (
-    <Logo/>
-  )
+    return ( <
+        Logo / >
+    )
 }
 
 export default AssistLogo
 
-export const Logo = styled.div`
+export const Logo = styled.div `
     height: 60px;
     width: 160px;
     background-image: url(${LogoAssist}) ;
     background-size: cover;
+
+    .HeaderLogo{
+      position: relative;
+      left: 40px;
+    }
 `
--- src/pages/homepage/HomePageElements.js
+++ src/pages/homepage/HomePageElements.js
@@ -1,5 +1,65 @@
 import styled from 'styled-components'
 
+
+
 export const HeaderElement = styled.div `
-    border: 1p solid red;   
+    border: 1px solid red;
+`
+
+export const ContainerElement = styled.form `
+    border: 1px solid black;
+    width: 100%;
+    text-align: center;
+    position: relative;
+    `
+export const DropdownElement = styled.button `
+    width:130px;
+    height:44px;
+    position: relative;
+    top: -50px;
+    left: 22px;
+    background-color: #fff;
+    cursor: pointer; 
+    border: 1px solid #D0D5DD;  
+    border-radius: 8px 0px 0px 8px; 
+    
+`
+export const SearchElement = styled.input `
+    width: 500px;
+    height: 44px;
+    position:relative;
+    top: -66px;
+    left: 20px;
+    border-radius:0px 8px 8px 0px;
+    border: 1px solid #D0D5DD;  
+`
+export const FavoriteBtnElement = styled.button `
+    width:100px;
+    height:44px;
+    position: relative;
+    top: -50px;
+    left:20px;
+    background-color: #fff;
+    cursor: pointer; 
+    border: 1px solid #D0D5DD;  
+   
+`
+export const UserElement = styled.button `
+    width:100px;
+    height:44px;
+    position: relative;
+    top: -50px;
+    left:20px;
+    background-color: #fff;
+    cursor: pointer; 
+    border: 1px solid #D0D5DD;  
+   
+`
+export const LabelElement = styled.h1 `
+    font-size: 2rem;
+    color: #D0D5DD;
+`
+
+export const SearchComponentElement = styled.div `
+    border: 1px solid red;
 `
