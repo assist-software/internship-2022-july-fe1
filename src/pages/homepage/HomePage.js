bbfb78f2718d09d1accf4c2d47351adc9790c3c0
anndy anndy <isar_anndy@yahoo.com>
2022-07-14 13:09:40 +0300

    Fixing another warnings from homepage

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,9 +1,3 @@
-import { HeaderElement, ContainerElement, DropdownElement, SearchElement, FavoriteBtnElement, LabelElement, UserElement, SearchComponentElement, FavouritesIcon } from './HomePageElements'
-import AssistLogo from '../../components/AssistLogo'
-import FormLabel from '../../components/FormLabel'
-import AssistLogoHeader from '../../components/AssistLogoHeader'
-import { FiChevronDown, FiHeart, FiUser, FiSearch } from 'react-icons/fi'
-import { TxtLabel } from '../../components/TxtLabel'
 import Header from '../../components/header/header'
 
 const HomePage = () => {
