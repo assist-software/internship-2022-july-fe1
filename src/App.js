d2bca8e6c7cc6601490ce693484bc221d7c599a4
EmiUngurean <emiungurean1@gmail.com>
2022-07-21 15:21:42 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 
-// import HomePage from './pages/Homepage/Homepage';
 import HomePage from './pages/Homepage/HomePage';
 import OnBoarding from './pages/OnBoarding/OnBoarding';
 import Profile from './pages/Profile/Profile';
@@ -22,6 +21,7 @@ const GlobalStyle = createGlobalStyle`
     font-weight: 600;
   }
 `;
+console.log('asd');
 
 function App() {
   return (
@@ -31,26 +31,26 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<HomePage />} />{' '}
-          <Route path='/category/:galeryname' element={<Galery />} />
+          <Route path='/category/:galeryname' element={<Galery />} />{' '}
           {/* ----------Protected Routed for authenticated user --------------- */}{' '}
           <Route element={<ProtectedRoutes />}>
             <Route path='/favourites' element={<HomePage />} />{' '}
-            <Route path='/pending' element={<HomePage />} />
+            <Route path='/pending' element={<HomePage />} />{' '}
             <Route path='/mylisting' element={<HomePage />} />{' '}
             <Route path='/mylisting/add' element={<HomePage />} />{' '}
             <Route path='/mylisting/:id' element={<HomePage />} />{' '}
-            <Route path='/mylisting/edit' element={<HomePage />} />
+            <Route path='/mylisting/edit' element={<HomePage />} />{' '}
             <Route path='/myprofile/profile' element={<Profile />} />{' '}
             <Route path='/myprofile/loginsecurity' element={<Profile />} />{' '}
             <Route path='/myprofile/notification' element={<Profile />} />{' '}
             <Route path='/myprofile/message' element={<Profile />} />{' '}
-          </Route>
-          <Route path='/login' element={<OnBoarding pageName='logIn' />} />
+          </Route>{' '}
+          <Route path='/login' element={<OnBoarding pageName='logIn' />} />{' '}
           <Route path='/sign-up' element={<OnBoarding pageName='createAccount' />} />{' '}
           <Route path='/forgot-password' element={<OnBoarding pageName='forgotPassword' />} />{' '}
           <Route path='/reset-password' element={<OnBoarding pageName='resetPassword' />} />{' '}
           {/* add new */} <Route path='/add-new' element={<AddNewPage />} />{' '}
-          <Route path='/editpost/:id' element={<AddNewPage />} /> {/* my account */}{' '}
+          <Route path='/editpost/:id' element={<AddNewPage />} /> {/ * my account * /}{' '}
           <Route path='/my-account' element={<MyAccountPage />} />{' '}
           <Route path='/my-account/profile' element={<MyAccountPage pageName='Profile' />} />{' '}
           <Route
